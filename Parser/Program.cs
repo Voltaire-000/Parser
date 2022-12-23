f38e2e50bbfa10e62316e341ddad1e1a568d5658
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2022-12-23 21:04:07 +0700

    in process

--- Parser/Program.cs
+++ Parser/Program.cs
@@ -29,9 +29,14 @@ namespace Parser
             string m_nextLine = streamReader.ReadLine();
             char separator = ' ';
             string[] m_line = m_nextLine.Split(separator);
-            string compoundName = m_line[0];
+            string compoundName = "\"" + m_line[0] + "\"" + ",";
             int startDescription = compoundName.Length;
             string m_description = m_nextLine.Substring(startDescription);
+            m_description = "\"" + m_description + "\"" + ",";
+            m_nextLine = streamReader.ReadLine();
+            string t_intervals = m_nextLine.Substring(0, 2);
+            string mzz = addQuotes(t_intervals);
+            string optionalId = m_nextLine.Substring(3, 9);
             Console.WriteLine(m_line[0]);
             
 
@@ -47,9 +52,10 @@ namespace Parser
             //TextFields m_description = delimitedFieldParser.ReadFields();
             parser.SetFieldWidths(2);
             TextFields m_name = parser.ReadFields();
-            string mm_name = "\"" + m_name.ToString() + "\"" + ",";
+            //string mm_name = "\"" + m_name.ToString() + "\"" + ",";
             string nameField = "\t\t\t\"" + "name" + "\"" + ":" + " ";
             string descriptionField = "\n\t\t\t\"" + "description" + "\"" + ":" + " ";
+            string t_intervalField  = "\n\t\t\t\"" + "t_intervals"  + "\"" + ":" + "";
             
             parser.SetFieldWidths(24);
             TextFields m_descr = parser.ReadFields();
@@ -65,10 +71,20 @@ namespace Parser
             streamWriter.WriteLine(" [");
             streamWriter.WriteLine("\t\t{");
             streamWriter.Write(nameField);
-            streamWriter.Write(mm_name);
+            streamWriter.Write(compoundName);
             streamWriter.Write(descriptionField);
+            streamWriter.Write(m_description);
+            streamWriter.Write(t_intervalField);
+            streamWriter.Write(t_intervals);
             streamWriter.Close();
             Console.Read();
+        }
+
+        private static string addQuotes(string t_intervals)
+        {
+            StringBuilder sb = new StringBuilder();
+            t_intervals =  sb.Insert(0, "\"").ToString();
+            return t_intervals;
         }
 
         private static string JsonStart(string m_line1, string m_thermo,string m_thermoField, StreamReader streamReader)
