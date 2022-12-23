a8bebb613ea2bf1e1527e34eedbad2decd13eeb1
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2022-12-23 12:37:56 +0700

    reading file

--- Parser/Program.cs
+++ Parser/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextFileParsers;
 
 namespace Parser
 {
@@ -10,6 +11,14 @@ namespace Parser
     {
         static void Main(string[] args)
         {
+            TextFileParsers.FixedWidthFieldParser parser = new TextFileParsers.FixedWidthFieldParser("..\\..\\thermo.inp");
+
+            //int[] fieldWidths = [0, 6]
+            parser.SetFieldWidths(6);
+            parser.IgnoreBlankLines = true;
+            TextFields tx =  parser.ReadFields();
+            string mstr = tx.ToString();
+
             Console.WriteLine(  "Hello world");
             Console.Read();
         }
