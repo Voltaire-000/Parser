3f2588371fde8e6717752120dd5290b56c7e0cc0
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2022-12-29 20:07:24 +0700

    add file

--- ElementZIP/Program.cs
+++ ElementZIP/Program.cs
@@ -10,15 +10,15 @@ namespace ElementZIP
         static void Main(string[] args)
         {
 
-            StreamReader elementReader = new StreamReader("..\\..\\element.txt");
+            StreamReader elementReader = new StreamReader("..\\..\\zip_Element.txt");
             StreamReader atomReader = new StreamReader("..\\..\\atomic.txt");
             //StreamReader valenceReader = new StreamReader("..\\..\\valence.txt");
 
-            StreamWriter elementWriter = new StreamWriter("..\\..\\zipElement.txt");
-            StreamWriter atomWriter = new StreamWriter("..\\..\\zipAtomic.txt");
+            //StreamWriter elementWriter = new StreamWriter("..\\..\\zipElement.txt");
+            //StreamWriter atomWriter = new StreamWriter("..\\..\\zipAtomic.txt");
             //StreamWriter valenceWriter = new StreamWriter("..\\..\\zipValence.txt");
 
-            atomWriter.AutoFlush = true;
+            //atomWriter.AutoFlush = true;
 
             while (!atomReader.EndOfStream)
             {
@@ -26,7 +26,7 @@ namespace ElementZIP
                 char oldChar = '\'';
                 char newChar = '"';
 
-                atomWriter.NewLine = ",";
+                //atomWriter.NewLine = ",";
 
 
                 string m_elements = elementReader.ReadToEnd();
@@ -40,7 +40,7 @@ namespace ElementZIP
 
             }
                 atomReader.Close();
-                atomWriter.Close();
+                //atomWriter.Close();
 
         }
     }
