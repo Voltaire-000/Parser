b39fec339ed87b9f9dba2d24870708bdc62905fd
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2022-12-29 15:30:27 +0700

    in process

--- jsonReader/Program.cs
+++ jsonReader/Program.cs
@@ -27,14 +27,34 @@ namespace jsonReader
             jsonDocument = JsonDocument.Parse(jsonString);
             JsonElement rootElement =  jsonDocument.RootElement;
             JsonElement thermo_element =  rootElement.GetProperty("thermo");
-.
-            //JsonElement r_element = thermo_element.GetProperty("reactant");
+            var x = thermo_element[1];
+            int thermolength = thermo_element.GetArrayLength();
+            var ch4 = thermo_element[7];
+            Type ch4Type = ch4.GetType();
+            JsonElement m_description;
+            JsonElement m_chemFor;
+            ch4.TryGetProperty("description", out m_description);
+            ch4.TryGetProperty("chemicalFormula", out m_chemFor);
+            string desc = m_description.ToString();
+            JsonElement numberOfCarbonAtoms = m_chemFor.GetProperty("C");
+            double c_num = numberOfCarbonAtoms.GetDouble();
+            JsonElement numberHydrogen = m_chemFor.GetProperty("H");
+            double h_num = numberHydrogen.GetDouble();
+            JsonElement m_weight;
+            ch4.TryGetProperty("molecularWeight", out m_weight);
+            double moleWeight = m_weight.GetDouble();
+
+            JsonElement m_tempRange = ch4.GetProperty("temperatureRange");
+
 
 
 
 
 
             Console.WriteLine("Hello world");
+
+
+            Console.Read();
             
         }
     }
