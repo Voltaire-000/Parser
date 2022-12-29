90baf4877c10c850d5d3e72aa4347fd47adcbc13
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2022-12-29 14:52:38 +0700

    in process

--- jsonReader/Program.cs
+++ jsonReader/Program.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
 
 
 namespace jsonReader
 {
+    public class Root
+    {
+        public List<Thermo> thermo { get; set; }
+
+    }
+    public class Thermo
+    {
+        public string reactant { get; set; }
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -12,7 +22,15 @@ namespace jsonReader
             
             //string file = "..\\..\\thermoINPjson.json";
             string file = "..\\..\\shortThermo.json";
-           
+            string jsonString = File.ReadAllText(file);
+            JsonDocument jsonDocument;
+            jsonDocument = JsonDocument.Parse(jsonString);
+            JsonElement rootElement =  jsonDocument.RootElement;
+            JsonElement thermo_element =  rootElement.GetProperty("thermo");
+.
+            //JsonElement r_element = thermo_element.GetProperty("reactant");
+
+
 
 
 
