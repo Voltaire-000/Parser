064904d034cf932f4d1cea5ca26c9e06e9150e01
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2022-12-30 19:34:01 +0700

    in process

--- transParser/Program.cs
+++ transParser/Program.cs
@@ -9,22 +9,27 @@ namespace transParser
     {
         static void Main(string[] args)
         {
-            UnicodeCategory unicodeCategory;
-
             StreamReader streamReader = new StreamReader("..\\..\\trans.inp");
             StreamWriter streamWriter = new StreamWriter("..\\..\\trans.json");
             streamWriter.AutoFlush = true;
 
-            string m_currentLine = "";
+            string rootName = "transport_property_coefficients";
+            bool printRoot = false;
 
-            streamWriter.Write("{");
-            streamWriter.WriteLine();
-            streamWriter.Write("\t" + "\"" + "transport_property_coefficients" + "\"" + ":" + " " + "[");
-            streamWriter.WriteLine();
-            //streamWriter.Write("\t\t\t\t\t" + "{");
+            string m_currentLine = "";
+            string m_symbol = "symbol";
+            string m_description = "description";
+            string m_viscosityLabel = "viscosity_coefficients";
 
             while (!streamReader.EndOfStream)
             {
+
+                if (!printRoot)
+                {
+                    printRoot = PrintRootName(streamWriter, rootName);
+
+                }
+
                 m_currentLine = streamReader.ReadLine();
 
                 // skip over first line
@@ -33,86 +38,59 @@ namespace transParser
                     m_currentLine = streamReader.ReadLine();
                 }
 
-                streamWriter.Write("\t\t\t\t");
-                streamWriter.Write("{");
-                streamWriter.WriteLine();
-
-                char m_char = m_currentLine.First();
-                unicodeCategory = char.GetUnicodeCategory(m_char);
-                Type type = m_char.GetType();
-                if (unicodeCategory != UnicodeCategory.LowercaseLetter || m_char == 'e')
-                {
-                    string m_speciesName = m_currentLine.Substring(0, 15);
-                    string m_description = m_currentLine.Substring(15);
-                    m_speciesName = m_speciesName.Trim();
-                    m_description = m_description.Trim();
-                    streamWriter.Write("\t\t\t\t\t");
-                    streamWriter.Write("\"" + "symbol" + "\"" + ": ");
-                    streamWriter.Write("\"" + m_speciesName + "\"" + ",");
-                    streamWriter.WriteLine();
-                    streamWriter.Write("\t\t\t\t\t");
-                    streamWriter.Write("\"" + "description" + "\"" + ": ");
-                    streamWriter.Write("\"" + m_description + "\"" + ",");
-                    m_currentLine = streamReader.ReadLine();
-                    m_char = m_currentLine.First();
-                    unicodeCategory = char.GetUnicodeCategory(m_char);
-                }
-                int vCount = 0;
-                if (unicodeCategory == UnicodeCategory.SpaceSeparator)
-                {
-                    char m_V = m_currentLine.ElementAt(1);
-                    if (m_V == 'V')
-                    {
-                        vCount = vCount + 1;
-                        //  we have viscosity coefficients
-                        streamWriter.WriteLine();
-                        streamWriter.Write("\t\t\t\t\t");
-                        streamWriter.Write("\"" + "viscosity_coefficients" + "\"" + ": " + "{");
-                        streamWriter.WriteLine();
-                        streamWriter.Write("\t\t\t\t\t\t\t");
-                        streamWriter.Write("\"" + "temperatureRange" + "\"" + ": " + "{");
-                        streamWriter.WriteLine();
-                        streamWriter.Write("\t\t\t\t\t\t\t\t\t");
-                        streamWriter.Write("\"" + "range_" + vCount.ToString() + "\"" + ": " + "{ ");
-                        streamWriter.WriteLine();
-                        streamWriter.Write("\t\t\t\t\t\t\t\t\t\t");
-                        streamWriter.Write("\"" + "temperatureRange" + "\"" + ":" + "[");
-                        string m_tempInterval_1 = m_currentLine.Substring(3, 6);
-                        streamWriter.Write(m_tempInterval_1 + ", ");
-                        string m_tempInterval_2 = m_currentLine.Substring(10, 8);
-                        m_tempInterval_2 = m_tempInterval_2.Trim();
-                        streamWriter.Write(m_tempInterval_2 + "]" + ",");
-                        streamWriter.WriteLine();
-                        streamWriter.Write("\t\t\t\t\t\t\t\t\t\t");
-                        streamWriter.Write("\"" + "coefficients" + "\"" + ":" + "[");
-                        string m_coeff = m_currentLine.Substring(21, 59);
-                        m_coeff = m_coeff.Replace('E', 'e');
-                        m_coeff = m_coeff.Replace("e ", "e+");
-                        m_coeff = m_coeff.Insert(14, " ");
-                        m_coeff = m_coeff.Replace(" ", ", ");
-                        
-                        streamWriter.Write(m_coeff + "]");
-                        streamWriter.WriteLine();
-                        streamWriter.Write("\t\t\t\t\t\t\t\t\t\t");
-                        streamWriter.Write("}" + ",");
-
-                        m_currentLine = streamReader.ReadLine();
-                        m_V = m_currentLine.ElementAt(1);
-
-                    }
-                }
-                {
-
-                    //streamWriter.Write
-                }
+                PrintNameAndDescription(streamWriter, m_currentLine, m_symbol, m_description);
+                GetFirstCharEachLine(streamWriter, streamReader, m_currentLine);
 
 
-                //break;
+                break;
 
             }
                 streamReader.Close();
                 streamWriter.Close();
 
+        }
+
+        private static void GetFirstCharEachLine(StreamWriter streamWriter, StreamReader streamReader, string m_currentLine)
+        {
+            
+        }
+
+        private static void PrintNameAndDescription(StreamWriter streamWriter, string m_currentLine, string m_symbol, string m_description)
+        {
+            UnicodeCategory unicodeCategory;
+            streamWriter.Write("\t\t\t\t");
+            streamWriter.Write("{");
+            streamWriter.WriteLine();
+
+            char m_char = m_currentLine.First();
+            unicodeCategory = char.GetUnicodeCategory(m_char);
+            Type type = m_char.GetType();
+            if (unicodeCategory != UnicodeCategory.LowercaseLetter || m_char == 'e')
+            {
+                m_symbol = m_currentLine.Substring(0, 15);
+                m_description = m_currentLine.Substring(15);
+                m_symbol = m_symbol.Trim();
+                m_description = m_description.Trim();
+                streamWriter.Write("\t\t\t\t\t");
+                streamWriter.Write("\"" + "symbol" + "\"" + ": ");
+                streamWriter.Write("\"" + m_symbol + "\"" + ",");
+                streamWriter.WriteLine();
+                streamWriter.Write("\t\t\t\t\t");
+                streamWriter.Write("\"" + "description" + "\"" + ": ");
+                streamWriter.Write("\"" + m_description + "\"" + ",");
+
+            }
+
+
+        }
+
+        private static bool PrintRootName(StreamWriter streamWriter, string fieldName)
+        {
+            streamWriter.Write("{");
+            streamWriter.WriteLine();
+            streamWriter.Write("\t" + "\"" + fieldName + "\"" + ":" + " " + "[");
+            streamWriter.WriteLine();
+            return true;
         }
     }
 }
