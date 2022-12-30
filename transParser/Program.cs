910ca15157315f8c941b4a0a89d0dc2013493947
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2022-12-30 16:22:47 +0700

    in process

--- transParser/Program.cs
+++ transParser/Program.cs
@@ -8,7 +8,36 @@ namespace transParser
     {
         static void Main(string[] args)
         {
-            StreamReader streamReader = new StreamReader("..\\..\\thermo.inp");
+            StreamReader streamReader = new StreamReader("..\\..\\trans.inp");
+            StreamWriter streamWriter = new StreamWriter("..\\..\\trans.json");
+            streamWriter.AutoFlush = true;
+
+            string m_currentLine = "";
+
+            streamWriter.Write("{");
+            streamWriter.WriteLine();
+            streamWriter.Write("\t" + "\"" + "transport_property_coefficients" + "\"" + ":" + " " + "[");
+            streamWriter.WriteLine();
+            //streamWriter.Write("\t\t\t\t\t" + "{");
+
+            while (!streamReader.EndOfStream)
+            {
+                m_currentLine = streamReader.ReadLine();
+
+                // skip over first line
+                if (m_currentLine.First() == 't')
+                {
+                    m_currentLine = streamReader.ReadLine();
+                }
+
+                streamWriter.WriteLine();
+
+                string m_subString = m_currentLine;
+
+            }
+                streamReader.Close();
+                streamWriter.Close();
+
         }
     }
 }
