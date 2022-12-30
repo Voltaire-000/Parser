136faa6b08fa207f01b50a204c7958b70ae43022
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2022-12-30 16:01:00 +0700

    add transparser

---
+++ transParser/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace transParser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
