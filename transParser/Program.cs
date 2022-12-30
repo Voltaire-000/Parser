31568eb0f364881062849f0396d64ef57151ed59
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2022-12-30 17:02:49 +0700

    in process

--- transParser/Program.cs
+++ transParser/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 
@@ -8,6 +9,8 @@ namespace transParser
     {
         static void Main(string[] args)
         {
+            UnicodeCategory unicodeCategory;
+
             StreamReader streamReader = new StreamReader("..\\..\\trans.inp");
             StreamWriter streamWriter = new StreamWriter("..\\..\\trans.json");
             streamWriter.AutoFlush = true;
@@ -34,13 +37,27 @@ namespace transParser
                 streamWriter.Write("{");
                 streamWriter.WriteLine();
 
+                string value;
+                char m_char = m_currentLine.First();
+                unicodeCategory = char.GetUnicodeCategory(m_char);
+                Type type = m_char.GetType();
+                //if ( m_currentLine.First() == m_char.
+                //{
+
+                //}
                 string m_speciesName = m_currentLine.Substring(0, 15);
+                string m_description = m_currentLine.Substring(15, 42);
                 m_speciesName = m_speciesName.Trim();
+                m_description = m_description.Trim();
                 streamWriter.Write("\t\t\t\t\t");
-                streamWriter.Write("\"" + m_speciesName + "\"" + ":");
+                streamWriter.Write("\"" + "symbol" + "\"" + ": ");
+                streamWriter.Write("\"" + m_speciesName + "\"" + ",");
                 streamWriter.WriteLine();
                 streamWriter.Write("\t\t\t\t\t");
-                streamWriter.Write("\"" + "description" + "\"");
+                streamWriter.Write("\"" + "description" + "\"" + ": ");
+                streamWriter.Write("\"" + m_description + "\"" + ",");
+
+                break;
 
             }
                 streamReader.Close();
