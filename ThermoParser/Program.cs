bdc5613d2610215345be9676d60781d282e9a130
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2023-01-04 10:44:05 +0700

    make ThermoParser project

---
+++ ThermoParser/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThermoParser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader streamReader = new StreamReader("..\\..\\thermo.inp");
+            StreamWriter streamWriter = new StreamWriter("..\\..\\thermoINP.json");
+        }
+    }
+}
