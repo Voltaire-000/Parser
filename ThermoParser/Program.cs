a70ffee1ef3ee3a21790c9c4c7c47cbc0cd1997f
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2023-01-04 10:50:52 +0700

    add switch

--- ThermoParser/Program.cs
+++ ThermoParser/Program.cs
@@ -9,10 +9,32 @@ namespace ThermoParser
 {
     internal class Program
     {
+        private static string m_currentLine = "";
+
         static void Main(string[] args)
         {
             StreamReader streamReader = new StreamReader("..\\..\\thermo.inp");
             StreamWriter streamWriter = new StreamWriter("..\\..\\thermoINP.json");
+            streamWriter.AutoFlush = true;
+
+            while (!streamReader.EndOfStream)
+            {
+                m_currentLine = streamReader.ReadLine();
+                DoSwitch(streamReader, streamWriter, m_currentLine);
+
+                if (streamReader.BaseStream == null)
+                {
+                    break;
+                }
+
+            }
+        }
+
+        private static void DoSwitch(StreamReader streamReader, StreamWriter streamWriter, string m_currentLine)
+        {
+            string rootLabel = "thermo";
+            //string rootLabel = "species";
+            
         }
     }
 }
