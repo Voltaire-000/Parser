9505a2df975611c3c655769894683a7db1b392ac
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2024-07-20 18:12:47 +0700

    in process

--- TransNew/Program.cs
+++ TransNew/Program.cs
@@ -17,65 +17,154 @@ namespace TransNew
         static void Main(string[] args)
         {
             string filePath = "..\\..\\trans.inp";
-            List<string> record = new List<string>();
-            List<double[]> temperatureRange = new List<double[]>();
-            List<double[]> viscosityCoeff = new List<double[]>();
-            try
-            {
-                bool hasViscosityCoefficient = false;
-                bool hasConductivityCoefficients = false;
-                int viscosityTempIntervals = 0;
-                int conductivityTemperatureIntervals = 0;
-                string[] lines = File.ReadAllLines(filePath);
-                foreach (string line in lines)
-                {
-                    if (line.Length < 80)
-                    {
-                        string speciesName = line.Substring(0, 15).Trim();
-                        string secondSpeciesName = line.Substring(16, 15).Trim();
-                        hasViscosityCoefficient = line[34] == 'V';
-                        viscosityTempIntervals = int.Parse(line[35].ToString());
-                        hasConductivityCoefficients = line[36] == 'C';
-                        conductivityTemperatureIntervals = int.Parse(line[37].ToString());
-                        string comments = line.Substring(40).Trim();
+            //List<string> record = new List<string>();
+            List<Record> records = new List<Record>();
+            string speciesName = "";
+            string secondSpeciesName = "";
+            string comments = "";
+            List<double[]> v_temperatureRange = new List<double[]>();
+            List<double[]> c_temperatureRange = new List<double[]>();
+            
+            
 
-                        record.Add(speciesName + " ," + secondSpeciesName + " " + comments);
-                    }
-                    else
+            int recordLineCount = 0;
+            bool isViscosity = false;
+            bool isConductivity = false;
+            bool hasViscosityCoefficient = false;
+            bool hasConductivityCoefficients = false;
+            int viscosityTempIntervals = 0;
+            int conductivityTemperatureIntervals = 0;
+            string[] lines = File.ReadAllLines(filePath);
+            var readlines = File.ReadLines(filePath);
+            var filestream = File.OpenRead(filePath);
+            StreamReader streamReader = new StreamReader("..\\..\\trans.inp");
+            string currentLine = "";
+            while (!streamReader.EndOfStream)
+            {
+                currentLine = streamReader.ReadLine();
+                if (currentLine.Length < 80)
+                {
+                    viscosityTempIntervals = int.Parse(currentLine[35].ToString());
+                    conductivityTemperatureIntervals = int.Parse(currentLine[37].ToString());
+                    recordLineCount = viscosityTempIntervals + conductivityTemperatureIntervals;
+                    speciesName = currentLine.Substring(0, 15).Trim();
+                    secondSpeciesName = currentLine.Substring(16, 15).Trim();
+                    comments = currentLine.Substring(40).Trim();
+                }
+                if (currentLine.Length >= 80)
+                {
+                    List<double[]> viscosityCoeff = new List<double[]>();
+                    List<double[]> conductivityCoeff = new List<double[]>();
+                    for (int i = 0; i < recordLineCount; i++)
                     {
-                        string coefficientLine = line.Substring(0, 2);
-                        bool isViscosity = coefficientLine[1] == 'V';
-                        // get the temperature
+                        double[] v_Coefflist = new double[4];
+                        double[] c_Coefflist = new double[4];
+
+                        string coefficientLine = currentLine.Substring(0, 2);
+                        isViscosity = coefficientLine[1] == 'V';
+                        isConductivity = coefficientLine[1] == 'C';
                         if (isViscosity)
                         {
-                            double firstTemperature = double.Parse(line.Substring(2, 9));
-                            double secondTemperature = double.Parse(line.Substring(9, 11));
-                            double[] templist = new double[2];
-                            templist[0] = firstTemperature;
-                            templist[1] = secondTemperature;
-                            temperatureRange.Add(templist);
-                            var firstCoeff = line.Substring(21, 14);
+                            double[] v_templist = new double[2];
+                            
+                            double firstTemperature = double.Parse(currentLine.Substring(2, 9));
+                            double secondTemperature = double.Parse(currentLine.Substring(9, 11));
+
+                            v_templist[0] = firstTemperature;
+                            v_templist[1] = secondTemperature;
+                            v_temperatureRange.Add(v_templist);
+                            var firstCoeff = currentLine.Substring(20, 15);
                             string cleanCoeffline = Regex.Replace(firstCoeff, @"([Ee])\s+", "$1");
                             cleanCoeffline = Regex.Replace(cleanCoeffline, @"([Ee])(?!\+|-)(\d)", "$1+$2");
                             double v_Coeff = double.Parse(cleanCoeffline, NumberStyles.Float, CultureInfo.InvariantCulture);
-                            double[] vCoeffList = new double[4];
-                            vCoeffList[0] = v_Coeff;
-
+                            v_Coefflist[0] = v_Coeff;
+                            var secondCoeff = currentLine.Substring(35, 15);
+                            cleanCoeffline = Regex.Replace(secondCoeff, @"([Ee])\s+", "$1");
+                            cleanCoeffline = Regex.Replace(cleanCoeffline, @"([Ee])(?!\+|-)(\d)", "$1+$2");
+                            v_Coeff = double.Parse(cleanCoeffline, NumberStyles.Float, CultureInfo.InvariantCulture);
+                            v_Coefflist[1] = v_Coeff;
+                            var thirdCoeff = currentLine.Substring(50, 15);
+                            cleanCoeffline = Regex.Replace(thirdCoeff, @"([Ee])\s+", "$1");
+                            cleanCoeffline = Regex.Replace(cleanCoeffline, @"([Ee])(?!\+|-)(\d)", "$1+$2");
+                            v_Coeff = double.Parse(cleanCoeffline, NumberStyles.Float, CultureInfo.InvariantCulture);
+                            v_Coefflist[2] = v_Coeff;
+                            var fourthCoeff = currentLine.Substring(65, 15);
+                            cleanCoeffline = Regex.Replace(fourthCoeff, @"([Ee])\s+", "$1");
+                            cleanCoeffline = Regex.Replace(cleanCoeffline, @"([Ee])(?!\+|-)(\d)", "$1+$2");
+                            v_Coeff = double.Parse(cleanCoeffline, NumberStyles.Float, CultureInfo.InvariantCulture);
+                            v_Coefflist[3] = v_Coeff;
+                            viscosityCoeff.Add(v_Coefflist);
+                            
                         }
+                        if (isConductivity)
+                        {
+                            double[] c_templist = new double[2];
+                            //double[] c_Coefflist = new double[4];
+                            double firstTemperature = double.Parse(currentLine.Substring(2, 9));
+                            double secondTemperature = double.Parse(currentLine.Substring(9, 11));
+                            c_templist[0] = firstTemperature;
+                            c_templist[1] = secondTemperature;
+                            c_temperatureRange.Add(c_templist);
+                            var firstCoeff = currentLine.Substring(20, 15);
+                            string cleanCoeffline = Regex.Replace(firstCoeff, @"([Ee])\s+", "$1");
+                            cleanCoeffline = Regex.Replace(cleanCoeffline, @"([Ee])(?!\+|-)(\d)", "$1+$2");
+                            double v_Coeff = double.Parse(cleanCoeffline, NumberStyles.Float, CultureInfo.InvariantCulture);
+                            c_Coefflist[0] = v_Coeff;
+                            var secondCoeff = currentLine.Substring(35, 15);
+                            cleanCoeffline = Regex.Replace(secondCoeff, @"([Ee])\s+", "$1");
+                            cleanCoeffline = Regex.Replace(cleanCoeffline, @"([Ee])(?!\+|-)(\d)", "$1+$2");
+                            v_Coeff = double.Parse(cleanCoeffline, NumberStyles.Float, CultureInfo.InvariantCulture);
+                            c_Coefflist[1] = v_Coeff;
+                            var thirdCoeff = currentLine.Substring(50, 15);
+                            cleanCoeffline = Regex.Replace(thirdCoeff, @"([Ee])\s+", "$1");
+                            cleanCoeffline = Regex.Replace(cleanCoeffline, @"([Ee])(?!\+|-)(\d)", "$1+$2");
+                            v_Coeff = double.Parse(cleanCoeffline, NumberStyles.Float, CultureInfo.InvariantCulture);
+                            c_Coefflist[2] = v_Coeff;
+                            var fourthCoeff = currentLine.Substring(65, 15);
+                            cleanCoeffline = Regex.Replace(fourthCoeff, @"([Ee])\s+", "$1");
+                            cleanCoeffline = Regex.Replace(cleanCoeffline, @"([Ee])(?!\+|-)(\d)", "$1+$2");
+                            v_Coeff = double.Parse(cleanCoeffline, NumberStyles.Float, CultureInfo.InvariantCulture);
+                            c_Coefflist[3] = v_Coeff;
+                            conductivityCoeff.Add(c_Coefflist);
+                        }
+                        // make new record here
+                        
+                        //currentLine = streamReader.ReadLine();
                     }
-       
+                    var record = BeginNewRecord(speciesName, secondSpeciesName, comments, v_temperatureRange, viscosityCoeff, c_temperatureRange, conductivityCoeff);
+                    records.Add(record);
+                    //currentLine = streamReader.ReadLine();
                 }
-                Console.WriteLine($"first line : { record}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error occured : {ex.Message}");
+                currentLine = streamReader.ReadLine();
             }
 
+        }
 
-
+        private static void MakeNewRecord(string line, int recordLineCount)
+        {
+            var speciesName = line.Substring(0, 15).Trim();
+            var secondSpeciesName = line.Substring(16, 15).Trim();
+            var comments = line.Substring(40).Trim();
             
+        }
 
+        private static Record BeginNewRecord(string speciesName, string secondSpeciesName, string comments,
+            List<double[]> vTemperatureRange, List<double[]> viscosityCoeff, List<double[]> cTemperatureRange, List<double[]> cCoefficients)
+        {
+            var record = new Record
+            {
+                SpeciesName = speciesName,
+                SecondSpeciesName = secondSpeciesName,
+                Comments = comments,
+
+                ViscosityTemperatureRange = vTemperatureRange,
+                ViscosityCoefficients = viscosityCoeff,
+
+                ConductivityTemperatureRange = cTemperatureRange,
+                ConductivityCoefficients = cCoefficients,
+
+            };
+            return record;
         }
     }
 }
--- TransNew/TextFileParser.cs
+++ TransNew/TextFileParser.cs
@@ -9,86 +9,25 @@ namespace TransNew
 {
     public class TextFileParser
     {
-        //    public class Record
-        //    {
-        //        public string SpeciesName { get; set; }
-        //        public string SecondSpeciesName { get; set; }
-        //        public bool HasViscosityCoefficient { get; set; }
-        //        public int ViscosityTemperatureIntervals { get; set; }
-        //        public bool HasThermalConductivityCoefficients { get; set; }
-        //        public int ConductivityTemperatureIntervals { get; set; }
-        //        public string Comments { get; set; }
-        //        public bool IsViscosity { get; set; }
-        //        public bool IsThermalConductivity { get; set; }
-        //        public double FirstTemperature { get; set; }
-        //        public double LastTemperature { get; set; }
-        //        public double[] Coefficients { get; set; }
-        //    }
-
-        //    public static Record ParseRecord(string line)
-        //    {
-        //        var speciesName = "";
-        //        var secondSpeciesName = "";
-        //        var hasViscosityCoefficient = false;
-        //        var viscosityTempIntervals = 0;
-        //        var hasConductivityCoefficients = false;
-        //        var conductivityTemperatureIntervals = 0;       
-        //        var comments = "";
-
-
-        //        if (line.Length < 80)
-        //        {
-        //            //throw new ArgumentException(" Line to short to contain all required fields");
-        //            speciesName = line.Substring(0, 15).Trim();
-        //            secondSpeciesName = line.Substring(16, 15).Trim();
-        //            hasViscosityCoefficient = line[34] == 'V';
-        //            viscosityTempIntervals = int.Parse(line[35].ToString());
-        //            hasConductivityCoefficients = line[36] == 'C';
-        //            conductivityTemperatureIntervals = int.Parse(line[37].ToString());
-        //            comments = line.Substring(40).Trim();
-        //        }
-        //        else
-        //        {
-        //            string coefficientLine = line.Substring(80);
-        //        }
-
-        //        var record = new Record
-        //        {
-        //            SpeciesName = speciesName,
-        //            SecondSpeciesName = secondSpeciesName,
-        //            HasViscosityCoefficient = hasViscosityCoefficient,
-        //            ViscosityTemperatureIntervals = viscosityTempIntervals,
-        //            HasThermalConductivityCoefficients = hasConductivityCoefficients,
-        //            ConductivityTemperatureIntervals = conductivityTemperatureIntervals,
-        //            Comments = comments,
-        //            IsViscosity = hasViscosityCoefficient,
-        //            FirstTemperature = 
-
-        //        };
-        //            if (line.Length >= 80)
-        //            {
-        //                string coefficientLine = line.Substring(80);
-        //                record.IsViscosity = coefficientLine[1] == 'V';
-        //            }
-        //        }
-
-
-        //        // parse coefficien line
-        //        if (line.Length >= 80)
-        //        {
-        //            string coefficientLine = line.Substring(80);
-        //            record.IsViscosity = coefficientLine[1] == 'V';
-        //            record.IsThermalConductivity = coefficientLine[1] == 'C';
-        //            record.FirstTemperature = double.Parse(coefficientLine.Substring(2, 9));
-        //            record.LastTemperature = double.Parse(coefficientLine.Substring(11, 9));
-        //            record.Coefficients = new double[4];
-        //            for (int i = 0; i < 4; i++)
-        //            {
-        //                record.Coefficients[i] = double.Parse(coefficientLine.Substring(20 + i * 15, 15));
-        //            }
-        //        }
-        //        return record;
-        //    }
-        //}
+        public class Record
+        {
+            public string SpeciesName { get; set; }
+            public string SecondSpeciesName { get; set; }
+            public string Comments { get; set; }
+            //public bool HasViscosityCoefficient { get; set; }
+            //public int ViscosityTemperatureIntervals { get; set; }
+            public List<double[]> ViscosityTemperatureRange { get; set; }
+            public List<double[]> ViscosityCoefficients { get; set; }
+            public List<double[]> ConductivityTemperatureRange { get; set; }
+            public List<double[]> ConductivityCoefficients { get; set; }
+            //public bool HasThermalConductivityCoefficients { get; set; }
+            //public int ConductivityTemperatureIntervals { get; set; }
+            
+            //public bool IsViscosity { get; set; }
+            //public bool IsThermalConductivity { get; set; }
+            //public double FirstTemperature { get; set; }
+            //public double LastTemperature { get; set; }
+            //public double[] Coefficients { get; set; }
+        }
     }
 }
